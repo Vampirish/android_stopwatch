a5a0ebe0f531deeef64fb54844dd868aafa2b025
Vampirish <vamperish@gmail.com>
2024-09-18 21:35:11 +0200

    connected the buttons

--- app/src/main/java/com/kehes/stopwatch/MainActivity.kt
+++ app/src/main/java/com/kehes/stopwatch/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.kehes.stopwatch
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kehes.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.e(this.javaClass.name, ">>> onCreate")
+        with(binding) {
+            pauseButton.setOnClickListener {
+                pauseClick()
+            }
+            startButton.setOnClickListener{
+                startClick()
+            }
+            resetButton.setOnClickListener {
+                resetClick()
+            }
         }
+    }
+
+    private fun pauseClick() {
+        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun startClick() {
+        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun resetClick() {
+        Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(this.javaClass.name, ">>> onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(this.javaClass.name, ">>> onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(this.javaClass.name, ">>> onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(this.javaClass.name, ">>> onDestroy")
     }
 }
