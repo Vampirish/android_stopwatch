750dd34fec06a1941bed1d312c6ce060addef27c
Vampirish <vamperish@gmail.com>
2024-09-19 20:12:38 +0200

    saves values after rotation

--- app/src/main/java/com/kehes/stopwatch/MainActivity.kt
+++ app/src/main/java/com/kehes/stopwatch/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kehes.stopwatch
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.BoringLayout
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -9,9 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kehes.stopwatch.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var seconds: Int = 0
+    private var running: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +36,46 @@ class MainActivity : AppCompatActivity() {
                 resetClick()
             }
         }
+
+        savedInstanceState?.let {
+            seconds = it.getInt(state.SECONDS.name)
+            running = it.getBoolean(state.RUNNING.name)
+        }
+
+        runTimer()
+    }
+
+    private fun runTimer() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.post(object : Runnable {
+            override fun run() {
+                val minutes = (seconds % 3600) / 60
+                val time = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+                binding.timeView.text = time
+
+                if (running) seconds++
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(state.SECONDS.name, seconds)
+        outState.putBoolean(state.RUNNING.name, running)
+        super.onSaveInstanceState(outState)
     }
 
     private fun pauseClick() {
-        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show()
+        running = false
     }
 
     private fun startClick() {
-        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show()
-
+        running = true
     }
 
     private fun resetClick() {
-        Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show()
-
+        seconds = 0
+        running = false
     }
 
     override fun onStart() {
@@ -64,4 +97,8 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.e(this.javaClass.name, ">>> onDestroy")
     }
+}
+
+enum class state{
+    RUNNING, SECONDS
 }
